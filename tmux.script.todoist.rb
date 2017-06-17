4689e68bdc53aea90519dd1f77a67a8c0777bcfc
Jorge A. Borges <contact@jorgeborges.me>
2017-06-17 20:50:47 +1000

    Update Todoist script to work with API v7 :smile_cat:

--- tmux.script.todoist.rb
+++ tmux.script.todoist.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
 
 # Gets all the account information from Todoist via API call
-# Keeps and uses a cache set to CACHE_EXPIRE_SECONDS
+# Keeps and uses a cache set to CACHE_EXPIRE_MINUTES
 # Outputs to Std something like ◯ 12 depending on the argument passed from console
 
-require 'rest_client'
+require 'rest-client'
+require 'json'
 require 'redis'
 require 'date'
 require 'yaml'
 
 TODOIST_KEY = 'todoist'
-CACHE_EXPIRE_SECONDS = 600
+CACHE_EXPIRE_MINUTES = 10
 
 config = YAML.load_file(__dir__ + '/config/todoist.yml')
 
@@ -19,7 +20,7 @@ cache_response = redis.get TODOIST_KEY
 
 if cache_response.nil?
   begin
-    response = RestClient.post 'https://api.todoist.com/TodoistSync/v5.3/get', :seq_no => '0', :api_token => config['api_token']
+    response = RestClient.post 'https://todoist.com/API/v7/sync', {token: config['api_token'], sync_token: '*', resource_types: '["all"]'}
   rescue
     if ARGV[0] == 'overdue'
       puts 'task_display()'
@@ -29,7 +30,7 @@ if cache_response.nil?
     Kernel.abort
   end
   redis.set TODOIST_KEY, response
-  redis.expire TODOIST_KEY, CACHE_EXPIRE_SECONDS
+  redis.expire TODOIST_KEY, (60 * CACHE_EXPIRE_MINUTES)
 else
   response = cache_response
 end
@@ -37,13 +38,13 @@ end
 todoist = JSON.parse(response)
 
 overdue = today = future = icebox = 0
-todoist['Items'].each do |item|
-  if item['due_date'].nil?
+todoist['items'].each do |item|
+  if item['due_date_utc'].nil?
     icebox += 1
   else
-    if Date.parse(item['due_date']) < Date.today
+    if Date.parse(item['due_date_utc']) < Date.today
       overdue += 1
-    elsif Date.parse(item['due_date']) == Date.today
+    elsif Date.parse(item['due_date_utc']) == Date.today
       today += 1
     else
       future += 1
@@ -52,10 +53,10 @@ todoist['Items'].each do |item|
 end
 
 totals = {
-    :overdue => '◯ ' + overdue.to_s,
-    :today => '✕ ' + today.to_s,
-    :future => ' ⃤ '+ future.to_s,
-    :icebox => '△ ' + icebox.to_s,
+  overdue: '◯ ' + overdue.to_s,
+  today: '✕ ' + today.to_s,
+  future: ' ⃤ ' + future.to_s,
+  icebox: '△ ' + icebox.to_s
 }
 
 puts totals[ARGV[0].to_sym]
