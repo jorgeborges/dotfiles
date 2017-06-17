97e90eece1d36de52385f9824db7026b58c8aab1
Jorge A. Borges <contact@jorgeborges.me>
2017-06-17 21:30:28 +1000

    Simplify ETH/AUD script

--- tmux.script.ethaud.rb
+++ tmux.script.ethaud.rb
@@ -9,21 +9,19 @@ ETH_PRICE_KEY = 'eth_aud_price'
 CACHE_EXPIRE_MINUTES = 15
 
 redis = Redis.new
-cache_price = redis.get ETH_PRICE_KEY
+price = redis.get ETH_PRICE_KEY
 
-if cache_price.nil?
+if price.nil?
   response = RestClient.get 'https://api.btcmarkets.net/market/ETH/AUD/tick',
                             accept: 'application/json'
   case response.code
     when 200
       market = JSON.parse(response)
-      current_price = market['lastPrice']
-      redis.set ETH_PRICE_KEY, current_price, ex: (60 * CACHE_EXPIRE_MINUTES)
+      price = market['lastPrice']
+      redis.set ETH_PRICE_KEY, price, ex: (60 * CACHE_EXPIRE_MINUTES)
     else
-      current_price = 'ERROR!'
+      price = 'err'
   end
-else
-  current_price = cache_price
 end
 
-puts '♦ ETH/AUD ' + current_price.to_s
+puts '♦ ETH/AUD ' + price.to_s
