05f771a13d277bd566e02c796956dcc93ffe7e63
Jorge A. Borges <contact@jorgeborges.me>
2021-11-14 17:59:16 +1100

    Fix script for new API and remove cache

--- tmux.script.todoist.rb
+++ tmux.script.todoist.rb
@@ -1,26 +1,17 @@
 #!/usr/bin/env ruby
 
 # Gets all the account information from Todoist via API call
-# Keeps and uses a cache set to CACHE_EXPIRE_MINUTES
 # Outputs to Std something like ◯ 12 depending on the argument passed from console
 
-require 'rest-client'
+require 'rest-client' # has to be gem installed
 require 'json'
-require 'redis'
 require 'date'
 require 'yaml'
 
-TODOIST_KEY = 'todoist'
-CACHE_EXPIRE_MINUTES = 5
-
 config = YAML.load_file(__dir__ + '/config/todoist.yml')
 
-redis = Redis.new
-response = redis.get TODOIST_KEY
-
-if response.nil?
   begin
-    response = RestClient.post 'https://todoist.com/API/v7/sync', {token: config['api_token'], sync_token: '*', resource_types: '["all"]'}
+    response = RestClient.get 'https://api.todoist.com/rest/v1/tasks', {:Authorization => 'Bearer ' + config['api_token']}
   rescue
     if ARGV[0] == 'overdue'
       puts 'task_display()'
@@ -28,20 +19,20 @@ if response.nil?
       puts '.'
     end
     Kernel.abort
-  end
-  redis.set TODOIST_KEY, response, ex: (60 * CACHE_EXPIRE_MINUTES)
 end
 
 todoist = JSON.parse response
 
 overdue = today = future = icebox = 0
-todoist['items'].each do |item|
-  if item['due_date_utc'].nil?
+todoist.each do |item|
+  next if item['completed']
+
+  if item['due'].nil?
     icebox += 1
   else
-    if Date.parse(item['due_date_utc']) < Date.today
+    if Date.parse(item['due']['date']) < Date.today
       overdue += 1
-    elsif Date.parse(item['due_date_utc']) == Date.today
+    elsif Date.parse(item['due']['date']) == Date.today
       today += 1
     else
       future += 1
@@ -52,7 +43,7 @@ end
 totals = {
   overdue: '◯ ' + overdue.to_s,
   today: '✕ ' + today.to_s,
-  future: ' ⃤ ' + future.to_s,
+  future: '□ ' + future.to_s,
   icebox: '△ ' + icebox.to_s
 }
 
