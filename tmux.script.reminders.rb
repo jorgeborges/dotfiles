81a30d67129a3414dabd0a6679b929d59fe6a1ce
Jorge A. Borges <contact@jorgeborges.me>
2026-09-25 11:12:18 +1000

    fix: drop emoji color mod

--- tmux.script.reminders.rb
+++ tmux.script.reminders.rb
@@ -6,7 +6,7 @@
 HEALTH_REMINDERS = [
   [7, '🌞 Morning!'],         # 7 AM
   [9, '🌞💧 Drink Water'],    # 9 AM
-  [10, '🌞🧘🏾 Stretch'],       # 10 AM
+  [10, '🌞🧘 Stretch'],       # 10 AM
   [11, '💧 Drink Water'],     # 11 AM
   [12, '🍽️  Lunch Time'],     # 12 PM
   [14, '🥤 Energy Drink'],    # 2 PM
